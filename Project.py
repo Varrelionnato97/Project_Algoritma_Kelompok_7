2d62a3d60aab417e1f2b4d11f0490a36080f2155
Varrel Aulya Fadhila <varrelionnato97@gmail.com>
2026-06-01 22:12:02 +0700

    Menambahkan fitur algoritma pada Project.py

--- Project.py
+++ Project.py
@@ -13,3 +13,18 @@ class Laptop:
         self.harga = int(harga)
         self.kategori = kategori
 
+# 2. Membuat Algoritma untuk mencari laptop
+def cari_binary_search(data_laptop, target_id):
+    low = 0
+    high = len(data_laptop) - 1
+    
+    while low <= high:
+        mid = (low + high) // 2
+        if data_laptop[mid].id == target_id:
+            return mid
+        elif data_laptop[mid].id < target_id:
+            low = mid + 1
+        else:
+            high = mid - 1
+            
+    return -1 # Jika tidak ditemukan
