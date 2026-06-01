467f1e066da39d715037a48d57d44e3265be43ea
rifkyjovani07 <rifkyjovani4@gmail.com>
2026-06-01 14:36:41 +0700

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,1 +1,15 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import csv
+import os
+
+class Laptop:
+    def __init__(self, id_laptop, merk, tipe, ram, storage, harga, kategori):
+        self.id = id_laptop
+        self.merk = merk
+        self.tipe = tipe
+        self.ram = int(ram)
+        self.storage = int(storage)
+        self.harga = int(harga)
+        self.kategori = kategori
 
